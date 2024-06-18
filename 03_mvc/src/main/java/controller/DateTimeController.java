aa7e15a5dc43382baf650ac2042b2b0691e2a491
gt-min <gt_min@naver.com>
2024-06-18 19:24:38 +0900

    20240618

---
+++ 03_mvc/src/main/java/controller/DateTimeController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.DateModel;
+
+public class DateTimeController extends HttpServlet {
+  
+  private static final long serialVersionUID = 1L;
+       
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  
+    // 요청 인코딩
+    request.setCharacterEncoding("UTF-8");
+    
+    // 요청 확인 (요청 파라미터 type 확인)
+    String type = request.getParameter("type");
+  
+    // 요청에 따른 비즈니스 로직 처리
+    String path = null;
+    switch(type) {
+    case "date":
+      DateModel dateModel = new DateModel();
+      path = dateModel.execute(request);  // Model 에 요청(request)을 전달한다.
+      break;
+    case "time":
+      System.out.println(type);      
+      break;
+    }
+    
+    // 응답 (결과를 보여 줄 JSP 로 이동하기 : 요청에 저장된 결과를 전달하기 위해서 forward 를 해야 한다.)
+    if(path != null) {
+      request.getRequestDispatcher(path).forward(request, response);
+    }
+  
+  }
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doGet(request, response);
+  }
+
+}
